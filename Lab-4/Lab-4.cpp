7a72c16105431f6ee927a3b124d5f6936b6583e3
MilenaBelica <milena.belica@outlook.com>
2025-11-16 14:35:16 +0100

    zmiany

--- Lab-4/Lab-4.cpp
+++ Lab-4/Lab-4.cpp
@@ -123,69 +123,6 @@ int main() {
 		else
 			cout << (k / 2) << endl;
 	}
-
-	//program
-		float fahr, celsius;
-		for (fahr = 0.0; fahr <= 200; fahr = fahr + 20) {
-			celsius = FtoC(fahr);
-			cout << fahr << "   " << celsius << endl;
-		}
-
-		float fahr1, kelvin;
-		for (fahr1 = 0.0; fahr1 <= 200; fahr1 = fahr + 20) {
-			kelvin = FtoK(fahr1);
-			cout << fahr << "   " << kelvin << endl;
-		}
-
-		float celsius2, fahr2;
-		for (celsius2 = 0.0; celsius2 <= 200; celsius2 += 20) {
-			fahr = CtoF(celsius2);
-			cout << fahr2 << "   " << celsius2 << endl;
-		}
-
-		float celsius3, kelvin2;
-		for (celsius3 = 0.0; celsius3 <= 200; celsius3 += 20) {
-			kelvin2 = CtoK(celsius3);
-			cout << kelvin2 << "   " << celsius3 << endl;
-		}
-
-		float kelvin4, celsius4;
-		for (kelvin4 = 0; kelvin4 <= 200; kelvin4 += 20) {
-			celsius4 = KtoC(kelvin4);
-			cout << kelvin4 << "   " << celsius4 << endl;
-		}
-
-		float kelvin5, fahr5;
-		for (kelvin5 = 0; kelvin5 <= 200; kelvin5 += 20) {
-			fahr5 = KtoC(kelvin5);
-			cout << kelvin4 << "   " << celsius4 << endl;
-		}
-		
-
-}
-float KtoF(float kelvin5) {
-	float wynik = (kelvin5 * 9.0 / 5.0 - 459.67);
-		return wynik;
-}
-float KtoC(float kelvin4){
-	float wynik = (kelvin4 - 273.15);
-	return wynik;
-}
-float CtoK(float celsius3) {
-	float wynik = (celsius3 + 273.15);
-	return wynik;
-}
-float CtoF(float celsius2) { 
-	float wynik = (celsius2 * 9.0 / 5.0) + 32.0;
-	return wynik;
-}
-float FtoK(float fahr1) {
-	float wynik = (5.0 / 9.0) * (fahr1 + 459.67);
-		return wynik;
-}
-float FtoC(float fahr) {
-	float wynik = (5.0 / 9.0) * (fahr - 32.0);
-	return wynik;
 }
 
 int zad7(int sz) {
